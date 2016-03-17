37b4fe932d0d8320e80b3cf79883f4a1edeb9659
dhoman01 <dustin.e.homan@gmail.com>
2016-03-17 00:56:24 -0600

    Finished

--- dna-align/src/main.cpp
+++ dna-align/src/main.cpp
@@ -9,24 +9,214 @@
 #include <cmath>
 #include <map>
 #include <iomanip>
-
-int match(char a, char b);
-
-std::string Hirshberg(std::string& X, std::string& Y);
-int partition(std::vector<int>& scoreL, std::vector<int>& scoreR);
-
-std::string dp_traceback(std::vector<std::vector<int>> cache, int i, int j, std::string& A, std::string& B);
-
-// NW algorithm. Calls cp_traceback
-std::string dp_align(std::string& a, std::string& b);
-
-// Equivalent to NWScore
-std::vector<int> dp_align_opt(std::string& a, std::string& b);
+#include "alignment.hpp"
+#include <thread>
+#include <unistd.h>
+#include <time.h>
 
 std::string read_mDNA(std::string filename);
 void write_alignment(std::string filename, std::string& alignment);
 
-void compareTo(std::string source_file, int numberOfFiles, std::string& source_mdna);
+// void compareTo(std::string source_file, int numberOfFiles, std::string& source_mdna);
+
+void pairWise(std::vector<std::string> source_files);
+
+void doSpaceTest();
+
+void compareHumanToNeandertal();
+void compareHumanDiversity();
+void compareHumanToGreatApes();
+
+void doPrintRunningTime(std::chrono::duration<double> child_runningtime);
+void printPairWiseResults(std::map<std::string, std::map<std::string,int>> scores);
+
+std::string fileToNameMap(std::string filename);
+
+template<typename T>
+void printElement(char align, T t, const int& width, char fill)
+{
+    std::cout << (align == 'L' ? std::left : std::right) << std::setw(width) << std::setfill(fill) << t;
+}
+
+void domDNAStudy()
+{
+  auto start = std::chrono::steady_clock::now();
+  std::cout << "Proto-Human to Neandertal" << std::endl;
+  printElement('L',"+", 50,'+');
+  std::cout << std::endl;
+  compareHumanToNeandertal();
+  // compareTo("../src/neandertal/neandertal",1,proto_human);
+  std::cout << "\nHuman Diversity" << std::endl;
+  printElement('L',"+", 50,'+');
+  std::cout << std::endl;
+  compareHumanDiversity();
+  // compareTo("../src/human/human",10,proto_human);
+  std::cout << "\nProto-Human to the Great Apes " << std::endl;
+  printElement('L',"+", 50,'+');
+  std::cout << std::endl;
+  compareHumanToGreatApes();
+  // compareTo("../src/great-apes/ape",6,proto_human);
+  auto end = std::chrono::steady_clock::now();
+
+  doPrintRunningTime(end - start);
+}
+
+int main(int argc, char* argv[])
+{
+  srand(time(NULL));
+  if(argc > 1)
+  {
+    if(std::string(argv[1]) == "-f")
+    {
+      auto file = fopen("log.out", "w");
+
+      dup2(fileno(file),STDOUT_FILENO);
+    }
+    else
+    {
+      std::cerr << "-f write STDOUT to log file instead of console" << std::endl;
+      return EXIT_FAILURE;
+    }
+  }
+
+  doSpaceTest();
+  domDNAStudy();
+
+  return EXIT_SUCCESS;
+}
+
+void doSpaceTest()
+{
+  for(int i = 1; i < 10; i++)
+  {
+    try
+    {
+      float size = std::pow(10.0,i);
+      std::cout << "Doing test with sequence size 10^" << i << std::endl;
+      align::test_upper_bound(size);
+    }
+    catch (std::bad_alloc& badAlloc)
+    {
+      std::cout << "Caught bad_alloc exception at size 10^" << i << " " << badAlloc.what() << std::endl;
+    }
+
+  }
+}
+
+void pairWise(std::vector<std::string> source_files)
+{
+  // Map to store the pairwise scores
+  std::map<std::string,std::map<std::string,int>> scores;
+
+  // iterate over each source file and compare it to
+  // all other files including its self
+  std::for_each(source_files.begin(), source_files.end(),[&](auto& filename){
+    std::string sequence1 = read_mDNA(filename);
+    std::string fn1 = fileToNameMap(filename);
+    std::for_each(source_files.begin(), source_files.end(),[&](auto& filename2){
+      std::string sequence2 = read_mDNA(filename2);
+      std::string fn2 = fileToNameMap(filename2);
+      std::vector<int> score = align::dp_align_opt(sequence1,sequence2);
+      scores[fn1][fn2] = score[sequence2.size()];
+    });
+  });
+
+  printPairWiseResults(scores);
+}
+
+void printPairWiseResults(std::map<std::string,std::map<std::string,int>> scores)
+{
+  std::cout << "\n" << std::endl;
+  std::cout << "Pairwise comparison" << std::endl;
+  printElement('R'," ", 11, ' ');
+  std::cout << "| ";
+  std::for_each(scores.begin(), scores.end(),[](auto it){
+    printElement('R',it.first, 11, ' ');
+  });
+
+  std::cout << std::endl;
+  printElement('R',"-", (scores.size() + 1) * 11, '-');
+  std::cout << std::endl;
+
+  std::for_each(scores.begin(),scores.end(),[&scores](auto it){
+    printElement('L',it.first,11, ' ');
+    std::cout << "|";
+    std::for_each(it.second.begin(),it.second.end(),[](auto it){
+      printElement('R',it.second,11, ' ');
+    });
+    std::cout << std::endl;
+  });
+}
+
+void compareHumanToNeandertal()
+{
+  std::string proto_human = read_mDNA("../src/proto-human.mdna");
+  std::string neandertal = read_mDNA("../src/neandertal/neandertal-1.mdna");
+  std::vector<int> score = align::dp_align_opt(proto_human,neandertal);
+  std::string alignment = align::Hirshberg(proto_human, neandertal);
+
+  // Write alignment to file asychronously
+  std::thread t([alignment](){
+    std::string align = alignment;
+    write_alignment("../src/neandertal/neandertal-1.mdna.alignment", align);
+  });
+  t.detach();
+
+  std::cout << "Proto-Human to Neandertal score: " << score[neandertal.size()] << std::endl;
+}
+
+void compareHumanDiversity()
+{
+  // Build list of filenames
+  std::vector<std::string> filenames;
+  for(int i = 1; i <= 10; i++)
+  {
+    std::stringstream ss;
+    ss << "../src/human/human-" << i << ".mdna";
+    filenames.push_back(ss.str());
+  }
+
+  // Do pairwise comparison
+  pairWise(filenames);
+}
+
+void compareHumanToGreatApes()
+{
+  // Build list of filenames
+  std::vector<std::string> filenames;
+  filenames.push_back("../src/proto-human.mdna");
+  for(int i = 1; i <= 6; i++)
+  {
+    std::stringstream ss;
+    ss << "../src/great-apes/ape-" << i << ".mdna";
+    filenames.push_back(ss.str());
+  }
+
+  // Do pairwise comparison
+  pairWise(filenames);
+
+  std::string proto_human = read_mDNA("../src/proto-human.mdna");
+  for(int i = 1; i <= 6; i++)
+  {
+    std::string filename = filenames[i];
+    std::string sequence = read_mDNA(filename);
+    std::string alignment = align::Hirshberg(proto_human,sequence);
+    filename.append(".alignment");
+
+    // Write alignment to file asychronously
+    // except for the last file, will join on that thread
+    std::thread t([filename,alignment](){
+      std::string align = alignment;
+      write_alignment(filename, align);
+    });
+    if(i < 6) t.detach();
+    else
+    {
+      std::cout << "\nWaiting on last file to write..." << std::endl;
+      t.join();
+    }
+  }
+}
 
 void doPrintRunningTime(std::chrono::duration<double> child_runningtime)
 {
@@ -37,254 +227,6 @@ void doPrintRunningTime(std::chrono::duration<double> child_runningtime)
   auto micro = std::chrono::duration_cast<std::chrono::microseconds>(child_runningtime);
   std::cout << "\n\n***     Time spent executing: " << sec.count() << " seconds ";
   std::cout << mill.count() << " milliseconds and " << micro.count() << " microseconds     ***" << std::endl;
-}
-
-template<typename T>
-void printElement(char align, T t, const int& width, char fill)
-{
-    std::cout << (align == 'L' ? std::left : std::right) << std::setw(width) << std::setfill(fill) << t;
-}
-
-std::string fileToNameMap(std::string filename);
-
-int main()
-{
-  std::string proto_human = read_mDNA("../src/proto-human.mdna");
-  // auto baseline_score = dp_align_opt(proto_human, proto_human);
-
-  // std::cout << "Baseline " << baseline_score[proto_human.size()] << std::endl;
-
-  auto start = std::chrono::steady_clock::now();
-  std::cout << "Proto-Human to Neandertal" << std::endl;
-  printElement('L',"+", 50,'+');
-  std::cout << std::endl;
-  compareTo("../src/neandertal/neandertal",1,proto_human);
-  std::cout << "\nProto-Human to Human Races" << std::endl;
-  printElement('L',"+", 50,'+');
-  std::cout << std::endl;
-  compareTo("../src/human/human",10,proto_human);
-  std::cout << "\nProto-Human to the Great Apes " << std::endl;
-  printElement('L',"+", 50,'+');
-  std::cout << std::endl;
-  compareTo("../src/great-apes/ape",6,proto_human);
-  auto end = std::chrono::steady_clock::now();
-
-  doPrintRunningTime(end - start);
-
-  return EXIT_SUCCESS;
-}
-
-int match(char a, char b)
-{
-  if(a == b) return 5;
-  else if(a == 'A' && b == 'G') return -2;
-  else if(a == 'A' && (b == 'C' || b == 'T')) return -1;
-  else if(a == 'A' && b == '-') return -3;
-  else if(a == 'C' && b == 'A') return -1;
-  else if(a == 'C' && b == 'G') return -3;
-  else if(a == 'C' && b == 'T') return -2;
-  else if(a == 'C' && b == '-') return -4;
-  else if(a == 'G' && (b == 'A' || b == 'T' || b == '-')) return -2;
-  else if(a == 'G' && b == 'C') return -3;
-  else if(a == 'T' && (b == 'A' || b == '-')) return -1;
-  else if(a == 'T' && (b == 'C' || b == 'G')) return -2;
-  else {
-    switch(b){
-      case 'A':
-        return -3;
-        break;
-      case 'C':
-        return -4;
-        break;
-      case 'G':
-        return -2;
-        break;
-      case 'T':
-        return -1;
-        break;
-    }
-  }
-  return -99999;
-}
-
-int partition(std::vector<int>& scoreL, std::vector<int>& scoreR)
-{
-  int i = 0;
-  int max = -999999;
-  int index = -1;
-  std::for_each(scoreR.rbegin(),scoreR.rend(),[&](auto score){
-    int sum = score + scoreL[i];
-    if(sum > max)
-    {
-      max = sum;
-      index = i;
-    }
-    i++;
-  });
-
-  return index;
-}
-
-std::string Hirshberg(std::string& X, std::string& Y)
-{
-  std::stringstream ss;
-  if(X.size() == 0)
-  {
-    for(int i = 0; i < Y.size(); i++)
-    {
-      ss << "- -> " << Y[i] << " (" << match('-',Y[i]) << ")" <<  std::endl;
-    }
-    return ss.str();
-  }
-  else if(Y.size() == 0)
-  {
-    for(int i = 0; i < X.size(); i++)
-    {
-      ss << X[i] << " -> - (" << match(X[i],'-') << ")" <<  std::endl;
-    }
-    return ss.str();
-  }
-  else if(X.size() == 1 || Y.size() == 1)
-  {
-    return dp_align(X,Y);
-  }
-  else
-  {
-    int xlen = X.size();
-    int xmid = std::ceil(X.size() / 2);
-    int ylen = Y.size();
-    auto lX = X.substr(0,xmid);
-    auto scoreL = dp_align_opt(lX,Y);
-    auto rX = X.substr(xmid);
-    auto rY = Y;
-    std::reverse(rX.begin(), rX.end());
-    std::reverse(rY.begin(), rY.end());
-    auto scoreR = dp_align_opt(rX, rY);
-
-    int ymid = partition(scoreL, scoreR);
-    auto lY = Y.substr(0,ymid);
-    rX = X.substr(xmid);
-    rY = Y.substr(ymid);
-    ss << Hirshberg(lX,lY) << Hirshberg(rX, rY);
-  }
-  return ss.str();
-}
-
-std::string dp_traceback(std::vector<std::vector<int>> cache, int i, int j, std::string& A, std::string& B)
-{
-  std::stringstream ss;
-  if(i < 1)
-  {
-    while(j > 0)
-    {
-      ss << "- -> " << B[j-1] << " (" << match('-',B[j-1]) << ")" << std::endl;
-      j--;
-    }
-    return ss.str();
-  }
-  if(j < 1)
-  {
-    while (i > 0)
-    {
-      ss << A[i-1] << " -> - (" << match(A[i-1],'-') << ")" <<  std::endl;
-      i--;
-    }
-    return ss.str();
-  }
-  int subD = cache[i-1][j] + match(A[i-1],'-');
-  int subI = cache[i][j-1] + match('-',B[j-1]);
-  int Swap = cache[i-1][j-1] + match(A[i-1], B[j-1]);
-  int max = std::max(subD,std::max(subI,Swap));
-  if(max == subD)
-  {
-    ss << dp_traceback(cache, i-1, j, A, B) << A[i-1] << " -> - (" << match(A[i-1],'-') << ")" <<  std::endl;
-    return ss.str();
-  }
-  else if(max == subI)
-  {
-    ss << dp_traceback(cache, i, j-1, A, B) << "- -> " << B[j-1] << " (" << match('-',B[j-1]) << ")" << std::endl;
-    return ss.str();
-  }
-  else if(max == Swap)
-  {
-    ss << dp_traceback(cache, i-1, j-1, A, B) << A[i-1] << " -> " << B[j-1] << " (" << match(A[i-1], B[j-1]) << ")" << std::endl;
-    return ss.str();
-  }
-  return ss.str();
-}
-
-std::string dp_align(std::string& a, std::string& b)
-{
-  std::vector<std::vector<int>> cache = std::vector<std::vector<int>>(a.size() + 1, std::vector<int>(b.size() + 1, 0));
-
-  for(int i = 0; i <= a.size(); i++)
-  {
-    for(int j = 0; j <= b.size(); j++)
-    {
-      if(i == 0)
-      {
-        int sum = 0;
-        for(int k = 0; k < j; k++)
-          sum += match('-', b[k]);
-        cache[0][j] = sum;
-      }
-      else if(j == 0)
-      {
-        int sum = 0;
-        for(int k = 0; k < i; k++)
-          sum += match(a[k], '-');
-        cache[i][0] = sum;
-      }
-      else
-      {
-        int subD = cache[i-1][j] + match(a[i-1], '-');
-        int subI = cache[i][j-1] + match('-', b[j-1]);
-        int Swap = cache[i-1][j-1] + match(a[i-1],b[j-1]);
-        int max = std::max(subD, std::max(subI, Swap));
-        cache[i][j] = max;
-      }
-    }
-  }
-
-  return dp_traceback(cache,a.size(),b.size(),a,b);
-}
-
-std::vector<int> dp_align_opt(std::string& a, std::string& b)
-{
-  std::vector<std::vector<int>> cache = std::vector<std::vector<int>>(2, std::vector<int>(b.size() + 1, 0));
-
-  for(int i = 0; i <= a.size(); i++)
-  {
-    for(int j = 0; j <= b.size(); j++)
-    {
-      if(i == 0)
-      {
-        int sum = 0;
-        for(int k = 0; k < j; k++)
-          sum += match('-', b[k]);
-        cache[1][j] = sum;
-      }
-      else if(j == 0)
-      {
-        int sum = 0;
-        for(int k = 0; k < i; k++)
-          sum += match(a[k], '-');
-        cache[1][0] = sum;
-      }
-      else
-      {
-        int subD = cache[0][j] + match(a[i-1], '-');
-        int subI = cache[1][j-1] + match('-', b[j-1]);
-        int Swap = cache[0][j-1] + match(a[i-1],b[j-1]);
-
-        int max = std::max(subD, std::max(subI, Swap));
-        cache[1][j] = max;
-      }
-    }
-    if(i < a.size())cache[0] = cache[1];
-  }
-
-  return cache[1];
 }
 
 std::string read_mDNA(std::string filename)
@@ -328,7 +270,8 @@ void write_alignment(std::string filename, std::string& alignment)
   file.close();
 }
 
-void compareTo(std::string source_file, int numberOfFiles, std::string& source_mdna)
+// Function used to compare a list of squences to a source sequence
+/*void compareTo(std::string source_file, int numberOfFiles, std::string& source_mdna)
 {
   std::map<std::string,int> scores;
   scores["Human"] = source_mdna.size() * 5;
@@ -378,10 +321,11 @@ void compareTo(std::string source_file, int numberOfFiles, std::string& source_m
     });
     std::cout << std::endl;
   });
-}
+}*/
 
 std::string fileToNameMap(std::string filename)
 {
+  if(filename.find("proto") != std::string::npos) return "Proto-Human";
   if(filename.find("neandertal") != std::string::npos) return "Neandertal";
   if(filename.find("human") != std::string::npos)
   {
